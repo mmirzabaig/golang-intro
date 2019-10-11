3a8fde119b2e7c7e7bba4fa88599d8dc7f24b5d5
Mirza Baig <mirzabaig@Mirzas-MBP.lan1>
2019-10-10 19:30:18 -0500

    #1

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(isOldEnoughToDrink(21))
+}
+
+// #1
+// Write a function called “isOldEnoughToDrink”.
+// Given a number, in this case an age, “isOldEnoughToDrink” returns whether a person of this given age is old enough to legally drink in the United States.
+// Notes:
+// * The legal drinking age in the United States is 21.
+
+func isOldEnoughToDrink(age int) bool {
+	if age >= 21 {
+		return true
+	}
+	return false
 }
